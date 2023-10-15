9f52596585d44cd18759ae6453a463120e8cad0a
chickenchickenlove <ojt90902@naver.com>
2023-10-15 13:55:04 +0900

    easy1 with erlang

--- src/easy/lc118.erl
+++ src/easy/lc118.erl
@@ -8,31 +8,23 @@
 -module(lc118).
 
 %% API
--export([]).
+-export([generate/1]).
 
 
 -spec generate(NumRows :: integer()) -> [[integer()]].
 generate(NumRows) ->
-1.
+  generate1(NumRows, [[1]]).
 
 % main 함수 짠다.
-generate1(NumRows, [H|T]) when NumRows > length([H|T]) ->
-  N0 =
+generate1(NumRows, Acc) when NumRows > length(Acc) ->
+  [H|_] = Acc,
+  generate1(NumRows, [gen(H, [1], 1)|Acc]);
+generate1(NumRows, Acc) when NumRows == length(Acc) ->
+  lists:reverse(Acc).
 
-
-
-
-  ;
-generate1(_, Acc) ->
-  Acc.
-
-
-
-% 꼬리 케이스들을 짠다
-
-
-
-
-
-
-
+% 가드조건이 있는 것부터 먼저 넣어줘야함. 패턴 매칭이 먼저 되는 것부터 하기 때문이다.
+gen([N0|T], Acc, Count) when length([N0|T]) >= 2 ->
+  [N1|_] = T,
+  gen(T, [N0 + N1|Acc], Count+1);
+gen(_N, Acc, _) ->
+  [1|Acc].
