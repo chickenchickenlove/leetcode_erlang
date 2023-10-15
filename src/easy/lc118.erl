033188a19e3cfbdb782d6dda7379bd89d85da3ff
ChickenchickenLove <ojt90902@naver.com>
2023-10-15 12:32:04 +0900

    temp

---
+++ src/easy/lc118.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. 10월 2023 오전 10:42
+%%%-------------------------------------------------------------------
+-module(lc118).
+
+%% API
+-export([]).
+
+
+-spec generate(NumRows :: integer()) -> [[integer()]].
+generate(NumRows) ->
+1.
+
+% main 함수 짠다.
+generate1(NumRows, [H|T]) when NumRows > length([H|T]) ->
+  N0 =
+
+
+
+
+  ;
+generate1(_, Acc) ->
+  Acc.
+
+
+
+% 꼬리 케이스들을 짠다
+
+
+
+
+
+
+
