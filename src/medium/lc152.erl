3e431f546c2b386dd6a48d18e71816b5e6e0e99f
ChickenchickenLove <ojt90902@naver.com>
2024-01-22 22:37:06 +0900

    medium 2

---
+++ src/medium/lc152.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. 1월 2024 오후 11:17
+%%%-------------------------------------------------------------------
+-module(lc152).
+
+%% API
+-export([max_product/1]).
+
+-spec max_product(Nums :: [integer()]) -> integer().
+max_product(Nums) ->
+  N = length(Nums),
+  case N of
+    0 -> 0;
+    1 ->
+      [H|_] = Nums,
+      H;
+    _ -> sol(0, 0, 0, Nums, 1, N)
+  end.
+
+sol(_, _, Result, _, Index, End) when Index > End ->
+  Result;
+sol(Max, Min, Result, [Current|T], Index, End) ->
+  NewMin = lists:min([Current, Min * Current, Max * Current]),
+  NewMax = lists:max([Current, Max * Current, Min * Current]),
+  NewResult = max(NewMax, Result),
+  sol(NewMax, NewMin, NewResult, T, Index+1, End).
+
+
+
+
+
+
--- src/medium/lc72.erl
+++ src/medium/lc72.erl
@@ -12,64 +12,48 @@
 
 -spec min_distance(Word1 :: unicode:unicode_binary(), Word2 :: unicode:unicode_binary()) -> integer().
 min_distance(Word1, Word2) ->
-  io:format("Word1: ~p, Word2: ~p ~n", [Word1, Word2]),
   W1 = length(binary_to_list(Word1)),
   W2 = length(binary_to_list(Word2)),
 
-  MapW1 = #{I => S || I <- lists:seq(1, W1) , S <- binary_to_list(Word1)},
-  MapW2 = #{I => S || I <- lists:seq(1, W2) , S <- binary_to_list(Word2)},
+  MapW1 = #{I => S || {I, S} <- lists:zip(lists:seq(1, W1), binary_to_list(Word1))},
+  MapW2 = #{I => S || {I, S} <- lists:zip(lists:seq(1, W2), binary_to_list(Word2))},
+  D = #{K => none || K <- lists:seq(1, W2)},
+  DP = #{K => D || K <- lists:seq(1, W1)},
+  {Result, _} = sol(MapW1, MapW2, DP),
+  Result.
 
-  lists:map(fun(I) ->
-    Name = list_to_atom(integer_to_list(I)),
-    ets:new(Name, [set, named_table]),
-    ets:insert(Name, list_to_tuple([none || _ <- lists:seq(1, W2)]))
-    end, lists:seq(1, W1)),
 
-  tear_down(W1).
+sol(MapW1, MapW2, Dp) ->
+  sol(MapW1, MapW2, maps:size(MapW1), maps:size(MapW2), Dp).
 
-sol([], Word2, I1, I2) ->
-  I2-I1;
-sol(Word1, [], I1, I2) ->
-  I1-I2;
-sol(Word1, Word2, I1, I2) ->
-  % Dp를 업데이트 하는 작업이 필요할 것 같음.
-  Value = case get_(I1, I2) of
-      none ->
-        case maps:get(I1, Word1) == maps:get(I2, Word2)  of
-            true -> sol(Word1, Word2, I1-1, I2-1);
-            _ ->
-              Insert = sol(Word1, Word2, I1, I2-1),
-              Delete = sol(Word1, Word2, I1-1, I2),
-              Replace = sol(Word1, Word2, I1-1, I2-1),
-              lists:min([Insert, Delete, Replace])
-        end;
-      V -> V
+sol(_, _, I1, I2, DP) when I1 == 0; I2 == 0->
+  {abs(I2-I1), DP};
+sol(W1, W2, I1, I2, DP) ->
+  {NewT1, NewDP} = case get(I1, I2, DP) of
+    none -> handle_none_case(W1, W2, I1, I2, DP);
+    V -> {V, DP}
   end,
-  update(I1, I2, Value),
-  Value.
+  UpdatedDp = update(I1, I2, NewT1, NewDP),
+  {NewT1, UpdatedDp}.
+
+handle_none_case(W1, W2, I1, I2, DP) ->
+  case maps:get(I1, W1) =:= maps:get(I2, W2) of
+    true -> sol(W1, W2, I1-1, I2-1, DP);
+    _ -> handle_diff_case(W1, W2, I1, I2, DP)
+  end.
+
+handle_diff_case(W1, W2, I1, I2, DP) ->
+  {T1, NewDp1} = sol(W1, W2, I1, I2-1, DP),
+  {T2, NewDp2} = sol(W1, W2, I1-1, I2, NewDp1),
+  {T3, NewDP3} = sol(W1, W2, I1-1, I2-1, NewDp2),
+  NewT = lists:min([T1, T2, T3]) + 1,
+  {NewT, NewDP3}.
 
 
-update(I1, I2, Value) ->
-  TableName = list_to_atom(integer_to_list(I1)),
-  Table0 = ets:lookup(TableName),
+get(I, J, Map) ->
+  maps:get(J, maps:get(I, Map)).
 
-
-
-  %% 이런 느낌으루다가...
-%%  ets:insert(Table, {1, "apple"}),
-%%  ets:insert(Table, {2, "banana"}),
-%%  ets:insert(Table, {3, "cherry"}),
-
-
-get_(I, J) ->
-  TableName = list_to_atom(integer_to_list(I)),
-  ets:lookup(TableName, J).
-
-
-
-
-
-tear_down(W1) ->
-  lists:map(fun(I) ->
-    Name = list_to_atom(integer_to_list(I)),
-    ets:delete(Name) end, lists:seq(1, W1)).
+update(I, J, Value, Map) ->
+  Row = maps:get(I, Map),
+  NewRow = maps:update(J, Value, Row),
+  maps:update(I, NewRow, Map).
