dddc198977a36578bc3dd3bc4f123f11fa5a57f5
ChickenchickenLove <ojt90902@naver.com>
2024-02-20 22:38:08 +0900

    medium 1

--- src/medium/lc72.erl
+++ src/medium/lc72.erl
@@ -15,8 +15,11 @@ min_distance(Word1, Word2) ->
   W1 = length(binary_to_list(Word1)),
   W2 = length(binary_to_list(Word2)),
 
-  MapW1 = #{I => S || {I, S} <- lists:zip(lists:seq(1, W1), binary_to_list(Word1))},
-  MapW2 = #{I => S || {I, S} <- lists:zip(lists:seq(1, W2), binary_to_list(Word2))},
+  F = fun({Index, Value}, Acc) ->
+    maps:put(Index, Value, Acc) end,
+
+  MapW1 = lists:foldl(F, #{}, lists:enumerate(binary_to_list(Word1))),
+  MapW2 = lists:foldl(F, #{}, lists:enumerate(binary_to_list(Word2))),
   D = #{K => none || K <- lists:seq(1, W2)},
   DP = #{K => D || K <- lists:seq(1, W1)},
   {Result, _} = sol(MapW1, MapW2, DP),
@@ -25,23 +28,17 @@ min_distance(Word1, Word2) ->
 
 sol(MapW1, MapW2, Dp) ->
   sol(MapW1, MapW2, maps:size(MapW1), maps:size(MapW2), Dp).
-
 sol(_, _, I1, I2, DP) when I1 == 0; I2 == 0->
   {abs(I2-I1), DP};
 sol(W1, W2, I1, I2, DP) ->
-  {NewT1, NewDP} = case get(I1, I2, DP) of
-    none -> handle_none_case(W1, W2, I1, I2, DP);
-    V -> {V, DP}
-  end,
+  {NewT1, NewDP} = case validate(I1, I2, W1, W2, DP) of
+                     {none, true}  -> sol(W1, W2, I1-1, I2-1, DP);
+                     {none, false} -> handle_diff_case(W1, W2, I1, I2, DP);
+                     {V, _} -> {V, DP}
+                   end,
   UpdatedDp = update(I1, I2, NewT1, NewDP),
   {NewT1, UpdatedDp}.
 
-handle_none_case(W1, W2, I1, I2, DP) ->
-  1,
-  case maps:get(I1, W1) =:= maps:get(I2, W2) of
-    true -> sol(W1, W2, I1-1, I2-1, DP);
-    _ -> handle_diff_case(W1, W2, I1, I2, DP)
-  end.
 
 handle_diff_case(W1, W2, I1, I2, DP) ->
   {T1, NewDp1} = sol(W1, W2, I1, I2-1, DP),
@@ -58,3 +55,8 @@ update(I, J, Value, Map) ->
   Row = maps:get(I, Map),
   NewRow = maps:update(J, Value, Row),
   maps:update(I, NewRow, Map).
+
+validate(I1, I2, W1, W2, DP) ->
+  First = get(I1, I2, DP),
+  Second = maps:get(I1, W1) =:= maps:get(I2, W2),
+  {First, Second}.
